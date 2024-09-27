d738dd7d0b2d77d3037aeb7e48b76406f4db92bd
Francis7575 <francis.albert.2009@gmail.com>
2024-09-26 22:41:39 -0700

    Client side setup with: React, Routes, Context, added header.tsx

---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Login } from "../pages/Login";
+export { default as Signup } from "../pages/Signup";
+export { default as Home } from "../pages/Home";
+export { default as Chat } from "../pages/Chat";
+export { default as Header } from "../components/Header";
+export { default as NotFound } from "../pages/NotFound";
