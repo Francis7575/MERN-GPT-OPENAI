8a2cd6a1b7ab71aa2a36c96db017ecfd0839effe
Francis7575 <francis.albert.2009@gmail.com>
2024-09-27 09:55:50 -0700

    added NavigationLink component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,52 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Logo from "./shared/Logo"
+import { useAuth } from "../context/authContext";
+import NavigationLink from "./shared/NavigationLink";
+import Box from '@mui/material/Box';
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <Box>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                text="Go To Chat"
+                to="/chat"
+                bg="#00fffc"
+                textColor="black"
+              />
+              <NavigationLink
+                text="logout"
+                to="/"
+                bg="#51538f"
+                textColor="white"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                text="Login"
+                to="/login"
+                bg="#00fffc"
+                textColor="black"
+              />
+              <NavigationLink
+                text="Signup"
+                to="/signup"
+                bg="#51538f"
+                textColor="white"
+              />
+            </>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   )
