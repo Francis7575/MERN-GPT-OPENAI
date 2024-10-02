4e90e458d57c234b87f5efddaf861ff948eef1d5
Francis7575 <francis.albert.2009@gmail.com>
2024-10-02 14:45:19 -0700

    updated App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
 import { RouterProvider } from "react-router-dom"
 import { Box } from "@mui/material";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { PAGE_DATA } from './utils/pageData'
 import LoadingPage from './pages/LoadingPage'
 
 const App = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 200);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Box sx={{ maxWidth: "1200px", margin: "0 auto" }}>
-      <Suspense fallback={<LoadingPage />}>
+      <Suspense fallback={loading ? <LoadingPage /> : null}>
         <RouterProvider router={PAGE_DATA} />
       </Suspense>
     </Box>
