b7f23a00d5aae97262441209d042658bdfb0e5b0
Francis7575 <francis.albert.2009@gmail.com>
2024-10-04 19:30:52 -0700

    added server-alive route on server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,17 @@
+import { Request, Response } from "express";
 import app from "./app";
 import { connectToDatabase } from "./db/connection"
 const PORT = process.env.PORT || 4000;
+
+// Added on cron-job to ping the server every 10 mins to avoid the server sleep on render for inactivity
+app.get("/server-alive", (req: Request, res: Response) => {
+  try {
+    res.status(200).send("Test route working");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
 
 connectToDatabase()
   .then(() => {
