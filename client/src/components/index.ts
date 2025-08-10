dc4bd445d748a10343ccf2aec202946dacb99a60
Francis7575 <francis.albert.2009@gmail.com>
2025-08-10 10:28:41 -0700

    updated

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Login } from "../pages/Login";
-export { default as Signup } from "../pages/Signup";
 export { default as Home } from "../pages/Home";
 export { default as Chat } from "../pages/Chat";
 export { default as Header } from "./shared/Header";
